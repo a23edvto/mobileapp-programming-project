e2b096d8c8e8dd5cf7c03a91aa3b5fef01862582
Edvaure04 <edvinht@gmail.com>
2024-05-06 17:07:32 +0200

    Added activity and changed to earlier gradle version

---
+++ app/src/main/java/com/example/project/AboutActivity.java
@@ -0,0 +1,16 @@
+package com.example.project;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
