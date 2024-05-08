5a20957d759d85cb42fa26737a1cfa26d92f0f41
Edvaure04 <edvinht@gmail.com>
2024-05-08 15:21:46 +0200

    Created two buttons that sends the player to the other activity

--- app/src/main/java/com/example/project/AboutActivity.java
+++ app/src/main/java/com/example/project/AboutActivity.java
@@ -1,6 +1,9 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.ViewCompat;
@@ -12,5 +15,14 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        Button button = findViewById(R.id.goBackButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AboutActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
